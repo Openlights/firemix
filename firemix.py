d80c309aeb54417e1f43b0ce485533027eb7f687
Jon Evans <jon@craftyjon.com>
2013-04-07 10:46:50 -0400

    Add profiler

--- firemix.py
+++ firemix.py
@@ -1,5 +1,8 @@
 import logging
 import inspect
+import signal
+import sys
+import yappi
 
 import presets
 from core.mixer import Mixer
@@ -7,9 +10,27 @@ from core.networking import Networking
 from core.scene_loader import SceneLoader
 
 
+ENABLE_PROFILING = True
+
+
+def sigint_handler(signum, frame):
+    global mixer
+    mixer.stop()
+    if ENABLE_PROFILING:
+        stats = yappi.get_stats(yappi.SORTTYPE_TTOT, yappi.SORTORDER_DESC, 10)
+        stats = [(s.name, s.ttot) for s in stats.func_stats]
+        print "\n------ PROFILING STATS ------"
+        for s in stats:
+            print "%s\t[%0.3f]" % (s[0], s[1])
+        print "-----------------------------"
+    sys.exit(0)
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     log = logging.getLogger("FireMix")
+
+    signal.signal(signal.SIGINT, sigint_handler)
 
     log.info("Booting FireMix...")
 
@@ -28,6 +49,9 @@ if __name__ == "__main__":
 
     log.info("The current preset is %s" % mixer.get_active_preset_name())
 
+    if ENABLE_PROFILING:
+        log.info("Starting profiler")
+        yappi.start()
     mixer.run()
 
     while True:
