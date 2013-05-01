05b014e37f46e2f9418f56aa8de87956656b3524
Jon Evans <jonathan.evans@olympusndt.com>
2013-05-01 08:57:12 -0400

    Register XMLRPC server

--- core/rpc_server.py
+++ core/rpc_server.py
@@ -8,6 +8,11 @@ class RPCServer(threading.Thread):
     def __init__(self, app, ip='127.0.0.1', port=8080):
         self._app = app
         self._flask = Flask(__name__)
+        self._handler = XMLRPCHandler('RPC2')
+        self._handler.connect(self._flask, '/RPC2')
+
+        self._handler.register(self.get_active_preset_name)
+
         threading.Thread.__init__(self)
 
     def run(self):
