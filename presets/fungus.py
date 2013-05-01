0b578146dfa13bd251266a3f8e2621ea908db527
Jon Evans <jonathan.evans@olympusndt.com>
2013-05-01 16:17:10 -0400

    More interesting Fungus

--- presets/fungus.py
+++ presets/fungus.py
@@ -23,14 +23,16 @@ class Fungus(RawPreset):
     _alive_color = (0.35, 1.0, 1.0)  # HSV
     _dead_color = (0.13, 0.87, 0.57)  # HSV
     _growth_time = 0.75  # seconds
-    _life_time = 4.0
+    _life_time = 5.0
     _isolated_life_time = 1.0
     _death_time = 1.75
     _fade_out_time = 0.25
     _spontaneous_birth_probability = 0.0001
-    _spread_probability = 0.021
-    _growth_spread_probability = 0.2
-    _pop_limit = 200
+    _spread_probability = 0.03
+    _growth_spread_probability = 0.13
+    _mass_destruction_time = 10.0
+    _mass_destruction_pop_threshold = 150
+    _pop_limit = 300
 
     # Internal parameters
     _time = {}
@@ -41,9 +43,10 @@ class Fungus(RawPreset):
 
     def draw(self, dt):
 
+        # Ensure that empty displays start up with some seeds
         p_birth = (1.0 - self._spontaneous_birth_probability) if self._pop > 5 else 0.5
 
-        # Birth
+        # Spontaneous birth: Rare after startup
         if (self._pop < self._pop_limit) and random.random() > p_birth:
             address = ( random.randint(0, self._max_strand - 1),
                         random.randint(0, self._max_fixture - 1),
@@ -75,15 +78,17 @@ class Fungus(RawPreset):
         # Lifetime
         for address in self._alive:
             neighbors = self.scene().get_pixel_neighbors(address)
+            live_neighbors = [i for i in neighbors if i in self._alive]
             lt = self._life_time
             if len(neighbors) == 0:
                 lt = self._isolated_life_time
 
-            if ((dt - self._time[address]) / lt) >= 1.0:
+            if len(live_neighbors) < 2 and ((dt - self._time[address]) / lt) >= 1.0:
                 self._alive.remove(address)
                 self._dying.append(address)
                 self._time[address] = dt
                 self._pop -= 1
+
             self._pixel_buffer[address] = self._alive_color_rgb
 
             # Spread
@@ -99,7 +104,6 @@ class Fungus(RawPreset):
         for address in self._dying:
             p, color = self._get_next_color(address, self._death_time, dt)
             if p >= 1.0:
-                #print "dead"
                 self._dying.remove(address)
                 self._fading_out.append(address)
                 self._time[address] = dt
@@ -109,15 +113,28 @@ class Fungus(RawPreset):
         for address in self._fading_out:
             p, color = self._get_next_color(address, self._fade_out_time, dt)
             if p >= 1.0:
-                #print "out"
                 self._fading_out.remove(address)
             self._pixel_buffer[address] = color
 
-    def _get_next_color(self, address, time_target, dt):
-        progress = (dt - self._time[address]) / time_target
+        # Mass destruction
+        if (self._pop == self._pop_limit) or \
+                (self._pop > self._mass_destruction_pop_threshold and ((dt % self._mass_destruction_time) == 0)):
+            for i in self._alive:
+                if random.random() > 0.75:
+                    self._alive.remove(i)
+                    self._dying.append(i)
+                    self._pop -= 1
+            for i in self._growing:
+                if random.random() > 0.75:
+                    self._growing.remove(i)
+                    self._dying.append(i)
+                    self._pop -= 1
 
-        #print "target", time_target
-        #print "progress", progress
+    def _get_next_color(self, address, time_target, dt):
+        """
+        Returns the next color for a pixel, given the pixel's current state
+        """
+        progress = (dt - self._time[address]) / time_target
 
         if progress > 1.0:
             progress = 1.0
@@ -129,9 +146,6 @@ class Fungus(RawPreset):
             idx += (1.0 / 3.0)
         elif time_target == self._fade_out_time:
             idx += (2.0 / 3.0)
-
-        #print "idx", idx
-        #print "color", self._fader.get_color(idx)
 
         return (progress, self._fader.get_color(idx))
 
