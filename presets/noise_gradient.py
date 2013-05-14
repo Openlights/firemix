9d5edf1f1e5568671dfa56d8235cdae1a805b977
Jon Evans <jonathan.evans@olympusndt.com>
2013-05-14 16:36:37 -0400

    WIP on NoiseGradient

--- presets/noise_gradient.py
+++ presets/noise_gradient.py
@@ -1,4 +1,3 @@
-import numpy as np
 import random
 
 from lib.raw_preset import RawPreset
@@ -18,11 +17,13 @@ class NoiseGradient(RawPreset):
 
         self.vertices = self.scene().get_intersection_points()
         random.shuffle(self.vertices)
-        self.vertices = self.vertices[:3]
+        self.vertices = self.vertices[:10]
 
         self.hues = {}
+        self.colors = {}
         self.pixels = self.scene().get_all_pixels()
         self.pixel_weights = {}
+        self.pixel_hues = {}
 
     def parameter_changed(self, parameter):
         pass
@@ -34,19 +35,17 @@ class NoiseGradient(RawPreset):
     def draw(self, dt):
         for pixel in self.pixels:
             # 32 ms/frame
-            hues, weights = zip(*[(self.hues[v], (1.0 / w)) for v, w in self.pixel_weights[pixel]])
-
-            # 70 ms/frame
-            hue = np.average(hues, weights=weights)
-            color = hsv_float_to_rgb_uint8((hue, 1.0, 1.0))
+            colors, weights = zip(*[(self.colors[v], w) for v, w in self.pixel_weights[pixel]])
+            color = self._weighted_average_color(colors, weights)
             self.setp(pixel, color)
 
-    def _create_gradient(self, threshold=550.0):
+    def _create_gradient(self, relax=5.0, threshold=250.0):
         # Pick a random hue at each vertex
         hmin = self.parameter('hue-min').get()
         hmax = self.parameter('hue-max').get()
         for v in self.vertices:
             self.hues[v] = random.uniform(hmin, hmax)
+            self.colors[v] = hsv_float_to_rgb_uint8((self.hues[v], 1.0, 1.0))
 
         for pixel in self.pixels:
             weights = []
@@ -56,6 +55,13 @@ class NoiseGradient(RawPreset):
                 if dist < 1.0:
                     dist = 1.0
                 if dist < threshold:
-                    weights.append((vertex, dist))
+                    weights.append((vertex, 1.0 / dist))
             self.pixel_weights[pixel] = weights
-            print len(weights)
+
+    def _weighted_average_color(self, colors, weights):
+        n = [(int(c[0] * w), int(c[1] * w), int(c[2] * w)) for c, w in zip(colors, weights)]
+        n = [sum(x) for x in zip(*n)]
+        d = sum(weights)
+        if d == 0:
+            return 0.0
+        return tuple([int(float(x) / d) for x in n])
