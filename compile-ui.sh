dd88998081e869c9518245e8cbd214a6624701ff
Jon Evans <jonathan.evans@olympusndt.com>
2013-06-05 11:16:07 -0400

    Begin work on settings GUI

--- compile-ui.sh
+++ compile-ui.sh
@@ -1,3 +1,4 @@
 pyside-uic ui/firemix.ui > ui/ui_firemix.py
 pyside-uic ui/dlg_add_preset.ui > ui/ui_dlg_add_preset.py
 pyside-uic ui/dlg_setup_networking.ui > ui/ui_dlg_setup_networking.py
+pyside-uic ui/dlg_settings.ui > ui/ui_dlg_settings.py
