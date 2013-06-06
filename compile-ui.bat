618ad807cba6c87c4d7c4cf17b7e6f179177e6e6
Jon Evans <jon@craftyjon.com>
2013-06-06 19:02:13 -0400

    Remove conflict markers

--- compile-ui.bat
+++ compile-ui.bat
@@ -2,9 +2,4 @@
 pyside-uic ui/firemix.ui > ui/ui_firemix.py
 pyside-uic ui/dlg_add_preset.ui > ui/ui_dlg_add_preset.py
 pyside-uic ui/dlg_setup_networking.ui > ui/ui_dlg_setup_networking.py
-<<<<<<< HEAD
 pyside-uic ui/dlg_settings.ui > ui/ui_dlg_settings.py
-exit
-=======
-
->>>>>>> origin/master
