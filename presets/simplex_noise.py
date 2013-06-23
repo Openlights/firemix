fe4197c24bf94bd126942409f493589066e952cc
Jesse Raymond <underwatercolor@gmail.com>
2013-06-23 15:30:52 -0400

    faster noise, stretched noise, spiral onset boost
    noise rewrit to use numpy arrays and flat buffer
    stretch parameter for noise stripes / rain
    speed boost onset for spiral

--- presets/simplex_noise.py
+++ presets/simplex_noise.py
@@ -1,4 +1,5 @@
 from noise import snoise3
+import numpy as np
 
 from lib.raw_preset import RawPreset
 from lib.parameters import FloatParameter, IntParameter
@@ -20,6 +21,7 @@ class SimplexNoise(RawPreset):
         self.add_parameter(FloatParameter('color-speed', 0.2))
         self.add_parameter(IntParameter('resolution', 128))
         self.add_parameter(FloatParameter('scale', 0.25))
+        self.add_parameter(FloatParameter('stretch', 1.0))
         self.add_parameter(FloatParameter('blackout', 0.25))
         self.add_parameter(FloatParameter('whiteout', 0.5))
         self.add_parameter(FloatParameter('luminance-scale', 0.75))
@@ -27,8 +29,7 @@ class SimplexNoise(RawPreset):
         self._offset_y = 0
         self._offset_z = 0
         
-        self.pixel_locations = self.scene().get_all_pixel_locations()
-        self.pixel_addresses = {}
+        self.pixel_locations = np.asarray(self.scene().get_all_pixel_locations())
 
         self.color_lookup = {}
         self._setup_pars()
@@ -51,15 +52,17 @@ class SimplexNoise(RawPreset):
             self._offset_z = -self._offset_z
             
         self._setup_pars()
-        self._offset_x += dt * self.parameter('speed').get() * math.cos(self.parameter('angle').get() * 2 * math.pi)
-        self._offset_y += dt * self.parameter('speed').get() * math.sin(self.parameter('angle').get() * 2 * math.pi)
+        angle = self.parameter('angle').get()
+        #self._offset_x += dt * self.parameter('speed').get() * math.cos(angle) * 2 * math.pi
+        #self._offset_y += dt * self.parameter('speed').get() * math.sin(angle) * 2 * math.pi
+        self._offset_x += dt * self.parameter('speed').get()
         self._offset_z += dt * self.parameter('color-speed').get()
         if self._mixer.is_onset():
             posterization = 2
         else:
             posterization = self.parameter('resolution').get()
 
-        luminance_table = []
+        luminance_table = np.zeros(self._luminance_steps)
         luminance = 0.0
         for input in range(self._luminance_steps):
             if input > self.parameter('blackout').get() * self._luminance_steps:
@@ -69,13 +72,26 @@ class SimplexNoise(RawPreset):
             else:
                 luminance = 0.5
             luminance = clip(0, luminance, 1.0)
-            luminance_table.append(math.floor(luminance * posterization) / posterization)
-            
-        
-        for pixel, location in enumerate(self.pixel_locations):
-            hue = (1.0 + snoise3(self.scale * location[0] + self._offset_x, self.scale * location[1] + self._offset_y, self._offset_z, 1, 0.5, 0.5)) / 2
-            hue = self.hue_min + ((math.floor(hue * posterization) / posterization) * (self.hue_max - self.hue_min))
-            
-            brightness = self._luminance_steps * (1.0 + snoise3(self.luminance_scale * location[0] + self._offset_x, self.luminance_scale * location[1] + self._offset_y, self._offset_z, 1, 0.5, 0.5)) / 2
-            
-            self.setPixelHLS(pixel, (hue, luminance_table[int(brightness)], 1.0))
+            luminance_table[input] = math.floor(luminance * posterization) / posterization
+
+        x, y = self.pixel_locations.T
+        dx = x
+        dy = y
+        x = dx * math.cos(angle) - dy * math.sin(angle)
+        y = dx * math.sin(angle) + dy * math.cos(angle)
+        x *= self.parameter('stretch').get()
+        x += self._offset_x
+        y += self._offset_y
+        locations = np.asarray([x,y]).T
+
+        hues = np.asarray([snoise3(self.scale * location[0], \
+                                   self.scale * location[1], \
+                                   self._offset_z, 1, 0.5, 0.5) for location in locations])
+        hues = (1.0 + hues) / 2
+        hues = self.hue_min + ((np.int_(hues * posterization) / float(posterization)) * (self.hue_max - self.hue_min))
+        brights = np.asarray([snoise3(self.luminance_scale * location[0], self.luminance_scale * location[1], self._offset_z, 1, 0.5, 0.5) for location in locations])
+        brights = (1.0 + brights) / 2
+        brights *= self._luminance_steps
+        luminances = luminance_table[np.int_(brights)]
+
+        self.setAllHLS(hues, luminances, 1.0)
--- presets/spiral.py
+++ presets/spiral.py
@@ -21,7 +21,8 @@ class SpiralGradient(RawPreset):
         self.add_parameter(FloatParameter('wave-hue-width', 0.1))        
         self.add_parameter(FloatParameter('wave-hue-period', 0.1))        
         self.add_parameter(FloatParameter('wave-speed', 0.1))        
-        self.add_parameter(FloatParameter('hue-step', 0.1))    
+        self.add_parameter(FloatParameter('onset-speed-boost', 5.0))
+        self.add_parameter(FloatParameter('onset-speed-decay', 1.0))
         self.add_parameter(HLSParameter('color-start', (0.0, 0.5, 1.0)))
         self.add_parameter(HLSParameter('color-end', (1.0, 0.5, 1.0)))
         self.add_parameter(FloatParameter('center-distance', 0.0))
@@ -32,14 +33,9 @@ class SpiralGradient(RawPreset):
 
         cx, cy = self.scene().center_point()
 
-        self.center_angle = 0
-        self.locations = np.asarray(self.scene().get_all_pixel_locations())
-        x,y = self.locations.T
-        x -= cx
-        y -= cy
-        self.pixel_distances = np.sqrt(np.square(x) + np.square(y))
-        self.pixel_angles = np.arctan2(y, x) / (2.0 * math.pi)
-        self.pixel_distances /= max(self.pixel_distances)
+        self.center_offset_angle = 0
+
+        self.onset_speed_boost = 1
 
         self.parameter_changed(None)
 
@@ -53,27 +49,28 @@ class SpiralGradient(RawPreset):
 
     def draw(self, dt):
         if self._mixer.is_onset():
-            self.hue_inner = self.hue_inner + self.parameter('hue-step').get()
+            self.onset_speed_boost = self.parameter('onset-speed-boost').get()
 
-        self.center_angle += dt * self.parameter('center-speed').get()
+        self.center_offset_angle += dt * self.parameter('center-speed').get() * self.onset_speed_boost
+        self.hue_inner += dt * self.parameter('hue-speed').get() * self.onset_speed_boost
+        self.wave_offset += dt * self.parameter('wave-speed').get() * self.onset_speed_boost
+        self.color_offset += dt * self.parameter('speed').get() * self.onset_speed_boost
 
-        cx, cy = self.scene().center_point()
-        self.locations = np.asarray(self.scene().get_all_pixel_locations())
-        x,y = self.locations.T
-        x -= cx + math.cos(self.center_angle) * self.parameter('center-distance').get()
-        y -= cy + math.sin(self.center_angle) * self.parameter('center-distance').get()
-        self.pixel_distances = np.sqrt(np.square(x) + np.square(y))
-        self.pixel_angles = np.arctan2(y, x) / (2.0 * math.pi)
-        self.pixel_distances /= max(self.pixel_distances)
-
-        self.hue_inner += dt * self.parameter('hue-speed').get()
-        self.wave_offset += dt * self.parameter('wave-speed').get()
-        self.color_offset += dt * self.parameter('speed').get()
+        self.onset_speed_boost = max(1, self.onset_speed_boost - self.parameter('onset-speed-decay').get())
 
         wave_hue_period = 2 * math.pi * self.parameter('wave-hue-period').get()
         wave_hue_width = self.parameter('wave-hue-width').get()
         radius_hue_width = self.parameter('radius-hue-width').get()
         angle_hue_width = self.parameter('angle-hue-width').get()
+
+        cx, cy = self.scene().center_point()
+        self.locations = np.asarray(self.scene().get_all_pixel_locations())
+        x,y = self.locations.T
+        x -= cx + math.cos(self.center_offset_angle) * self.parameter('center-distance').get()
+        y -= cy + math.sin(self.center_offset_angle) * self.parameter('center-distance').get()
+        self.pixel_distances = np.sqrt(np.square(x) + np.square(y))
+        self.pixel_angles = np.arctan2(y, x) / (2.0 * math.pi)
+        self.pixel_distances /= max(self.pixel_distances)
 
         angles = np.mod(1.0 + self.pixel_angles + np.sin(self.wave_offset + self.pixel_distances * wave_hue_period) * wave_hue_width, 1.0)
         hues = self.color_offset + (radius_hue_width * self.pixel_distances) + (2 * np.abs(angles - 0.5) * angle_hue_width)
