905d1c371bf1f0ecda5d4a76b104256980bc1292
RJ Ryan <rryan@mixxx.org>
2013-08-04 19:14:27 -0400

    Swap loop ordering in Networking.write to avoid re-processing strand data when multiple clients are enabled.
    (cherry picked from commit a04969367185499dbf37a2468fca6a1e69ba9d81)

--- core/networking.py
+++ core/networking.py
@@ -24,7 +24,6 @@ class Networking:
     def open_socket(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    @profile
     def write(self, buffer):
         """
         Performs a bulk strand write.
@@ -46,42 +45,57 @@ class Networking:
                     packet[buffer_index + 1] = pixel[1]
                     packet[buffer_index + 2] = pixel[2]
 
-        for client in (client for client in self._app.settings['networking']['clients'] if client["enabled"]):
-            # TODO: Split into smaller packets so that less-than-ideal networks will be OK
-            client_color_mode = client["color-mode"]
-            client_host_port = (client["host"], client["port"])
+        clients = [client for client in self._app.settings['networking']['clients']
+                   if client["enabled"]]
 
-            for strand in xrange(len(strand_settings)):
-                if not strand_settings[strand]["enabled"]:
-                    continue
-                packet = array.array('B', [])
+        if not clients:
+            return
 
-                color_mode = strand_settings[strand]["color-mode"]
-                start, end = BufferUtils.get_strand_extents(strand)
+        for strand in xrange(len(strand_settings)):
+            if not strand_settings[strand]["enabled"]:
+                continue
+            packet = array.array('B', [])
 
-                packet_header_size = 4
-                packet_size = (end-start) * 3 + packet_header_size
+            color_mode = strand_settings[strand]["color-mode"]
+            start, end = BufferUtils.get_strand_extents(strand)
 
-                packet = self._packet_cache.get(packet_size, None)
-                if packet is None:
-                    packet = [0,] * packet_size
-                    self._packet_cache[packet_size] = packet
+            packet_header_size = 4
+            packet_size = (end-start) * 3 + packet_header_size
 
-                command = COMMAND_SET_RGB if color_mode == "RGB8" else COMMAND_SET_BGR
-                packet[0] = strand
-                packet[1] = command
-                length = packet_size - packet_header_size
-                packet[2] = length & 0x00FF
-                packet[3] = (length & 0xFF00) >> 8
+            packet = self._packet_cache.get(packet_size, None)
+            if packet is None:
+                packet = [0,] * packet_size
+                self._packet_cache[packet_size] = packet
 
-                # TODO(rryan): Are there other color modes?
-                swap_order = client_color_mode == 'BGR8'
-                fill_packet(buffer_rgb, start, end, packet_header_size, packet, swap_order)
-                packet = array.array('B', packet)
+            command = COMMAND_SET_RGB if color_mode == "RGB8" else COMMAND_SET_BGR
+            packet[0] = strand
+            packet[1] = command
+            length = packet_size - packet_header_size
+            packet[2] = length & 0x00FF
+            packet[3] = (length & 0xFF00) >> 8
+
+            rgb8_packet = None
+            bgr8_packet = None
+
+            for client in clients:
+                # TODO: Split into smaller packets so that less-than-ideal networks will be OK
+                client_color_mode = client["color-mode"]
+                if client_color_mode == 'RGB8':
+                    if rgb8_packet is None:
+                        fill_packet(buffer_rgb, start, end, packet_header_size, packet, False)
+                        rgb8_packet = array.array('B', packet)
+                    packet = rgb8_packet
+                elif client_color_mode == 'BGR8':
+                    if bgr8_packet is None:
+                        fill_packet(buffer_rgb, start, end, packet_header_size, packet, True)
+                        bgr8_packet = array.array('B', packet)
+                    packet = rgb8_packet
+                else:
+                    raise NotImplementedError('Unknown color mode: %s' % client_color_mode)
 
                 try:
                     #print "Sending packet of length %i for strand %i", (len(packet), strand)
-                    self._socket.sendto(packet, client_host_port)
+                    self._socket.sendto(packet, (client["host"], client["port"]))
                 except IOError as (errno, strerror):
                     print "I/O error({0}): {1}".format(errno, strerror)
                     #print "On strand %i with length %i" % (strand, len(packet))
