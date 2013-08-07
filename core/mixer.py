fe048b586f77854452ef3410d2869aa82964c24f
RJ Ryan <rryan@mixxx.org>
2013-08-06 20:34:26 -0400

    Calculate actual time since last frame (dt) for tick instead of theoretical time. This keeps animations proceeding in real time even when the frame rate is lagging.
    (cherry picked from commit ad03624088b77fe1cdc9e6f797f8fd003ee4c51e)

--- core/mixer.py
+++ core/mixer.py
@@ -64,6 +64,7 @@ class Mixer(QtCore.QObject):
         self._global_dimmer = 1.0
         self._global_speed = 1.0
         self._render_in_progress = False
+        self._last_tick_time = time.time()
         self.transition_progress = 0.0
 
         if self._app.args.yappi and USE_YAPPI:
@@ -263,7 +264,9 @@ class Mixer(QtCore.QObject):
 
     def tick(self):
         self._num_frames += 1
-        dt = (self._global_speed / self._tick_rate)
+        now = time.time()
+        dt = now - self._last_tick_time
+        self._last_tick_time = now
         if len(self._playlist) > 0:
 
             self._playlist.get_active_preset().clear_commands()
