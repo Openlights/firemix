d5cedeb24409af2a3692cf7c7349d68af9fe6f9e
RJ Ryan <rryan@mixxx.org>
2013-08-18 01:05:20 -0700

    Fix the CombinePresets preset to not assume the preset being combined is a RawPreset.
    (cherry picked from commit 8179ce2f9205c0835a3272325e4cf64173b1b16f)

--- presets/combine.py
+++ presets/combine.py
@@ -1,3 +1,4 @@
+from lib.buffer_utils import BufferUtils
 from lib.raw_preset import RawPreset
 from lib.parameters import FloatParameter, HLSParameter, StringParameter
 
@@ -6,14 +7,16 @@ class CombinePresets(RawPreset):
     Preset that loads two presets and renders both
     Combine requires a transition that will render an arbitrary progress point
     """
-       
+
     def setup(self):
         self.add_parameter(StringParameter('first-preset', ""))
         self.add_parameter(StringParameter('second-preset', ""))
         self.add_parameter(FloatParameter('transition-progress', 0.5))
         self.add_parameter(StringParameter('transition-mode', "Additive Blend"))
         self.parameter_changed(None)
-    
+        self._preset1_buffer = BufferUtils.create_buffer()
+        self._preset2_buffer = BufferUtils.create_buffer()
+
     def parameter_changed(self, parameter):
         self._transition = self._mixer.get_transition_by_name(self.parameter('transition-mode').get())
         if self._transition:
@@ -34,4 +37,10 @@ class CombinePresets(RawPreset):
 
             preset1.tick(dt)
             preset2.tick(dt)
-            self._pixel_buffer = self._transition.get(preset1._pixel_buffer, preset2._pixel_buffer, self.parameter('transition-progress').get())
+
+            preset1_buffer = preset1.render_to_buffer(self._preset1_buffer)
+            preset2_buffer = preset2.render_to_buffer(self._preset2_buffer)
+
+            self._pixel_buffer = self._transition.get(
+                preset1_buffer, preset2_buffer,
+                self.parameter('transition-progress').get())
