0f41998bfa6bb8fe803c11b7813c09bb55843fac
Jon Evans <jon@craftyjon.com>
2015-05-22 19:30:30 -0400

    Fix GPL typos

--- lib/color_modes.py
+++ lib/color_modes.py
@@ -7,13 +7,13 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Foobar is distributed in the hope that it will be useful,
+# Firemix is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with Firemix.  If not, see <http://www.gnu.org/licenses/>.
 
 modes = ["RGB8", "HSVF32", "HLSF32"]
 
