3142a096fbfd8fcec111219f1db6e628e88c104e
Jon Evans <jon@craftyjon.com>
2015-05-22 23:26:29 -0400

    Refactor net code and add frame start/end messages

--- core/networking.py
+++ core/networking.py
@@ -28,6 +28,8 @@ from profilehooks import profile
 from lib.colors import hls_to_rgb
 from lib.buffer_utils import BufferUtils
 
+COMMAND_START_FRAME = 0x01
+COMMAND_END_FRAME = 0x02
 COMMAND_SET_BGR = 0x10
 COMMAND_SET_RGB = 0x20
 
@@ -53,23 +55,6 @@ class Networking:
         # Protect against presets or transitions that write float data.
         buffer_rgb = np.int_(hls_to_rgb(buffer) * 255)
 
-        for client in [client for client in self._app.settings['networking']['clients'] if client["enabled"]]:
-            # TODO: Split into smaller packets so that less-than-ideal networks will be OK
-            #packet = array.array('B', [])
-            client_color_mode = client["color-mode"]
-
-            for strand in range(len(strand_settings)):
-                packet = array.array('B', [])
-                if not strand_settings[strand]["enabled"]:
-                    continue
-                color_mode = strand_settings[strand]["color-mode"]
-
-                start, end = BufferUtils.get_strand_extents(strand)
-                #print strand, start, end
-
-                #if client_color_mode == "RGB8":
-                 #   data = array.array('B', alldata[start*3:end*3])
-
         def fill_packet(intbuffer, start, end, offset, packet, swap_order=False):
             for pixel_index, pixel in enumerate(intbuffer[start:end]):
                 buffer_index = offset + pixel_index * 3
@@ -88,51 +73,38 @@ class Networking:
         if not clients:
             return
 
+        packets = []
+        packets.append(array.array('B', [COMMAND_START_FRAME, 0, 0, 0, 0]))
+
         for strand in xrange(len(strand_settings)):
             if not strand_settings[strand]["enabled"]:
                 continue
-            packet = array.array('B', [])
+            array_packet = array.array('B', [])
 
-            color_mode = strand_settings[strand]["color-mode"]
             start, end = BufferUtils.get_strand_extents(strand)
 
             packet_header_size = 4
             packet_size = (end-start) * 3 + packet_header_size
 
-            packet = self._packet_cache.get(packet_size, None)
-            if packet is None:
-                packet = [0,] * packet_size
-                self._packet_cache[packet_size] = packet
+            packet = [0,] * packet_size
 
-            command = COMMAND_SET_RGB if color_mode == "RGB8" else COMMAND_SET_BGR
-            packet[0] = strand
-            packet[1] = command
+            packet[0] = COMMAND_SET_RGB
+            packet[1] = strand
             length = packet_size - packet_header_size
             packet[2] = length & 0x00FF
             packet[3] = (length & 0xFF00) >> 8
 
-            rgb8_packet = None
-            bgr8_packet = None
+            fill_packet(buffer_rgb, start, end, packet_header_size, packet, False)
+            array_packet.extend(array.array('B', packet))
+            packets.append(array_packet)
 
-            for client in clients:
-                # TODO: Split into smaller packets so that less-than-ideal networks will be OK
-                client_color_mode = client["color-mode"]
-                if client_color_mode == 'RGB8':
-                    if rgb8_packet is None:
-                        fill_packet(buffer_rgb, start, end, packet_header_size, packet, False)
-                        rgb8_packet = array.array('B', packet)
-                    packet = rgb8_packet
-                elif client_color_mode == 'BGR8':
-                    if bgr8_packet is None:
-                        fill_packet(buffer_rgb, start, end, packet_header_size, packet, True)
-                        bgr8_packet = array.array('B', packet)
-                    packet = rgb8_packet
-                else:
-                    raise NotImplementedError('Unknown color mode: %s' % client_color_mode)
+        packets.append(array.array('B', [COMMAND_END_FRAME, 0, 0, 0, 0]))
 
+        for client in [client for client in self._app.settings['networking']['clients'] if client["enabled"]]:
+            for p in packets:
                 try:
                     #print "Sending packet of length %i for strand %i" % (len(packet), strand)
-                    self._socket.sendto(packet, (client["host"], client["port"]))
+                    self._socket.sendto(p, (client["host"], client["port"]))
                 except IOError as (errno, strerror):
                     print "I/O error({0}): {1}".format(errno, strerror)
                     #print "On strand %i with length %i" % (strand, len(packet))
