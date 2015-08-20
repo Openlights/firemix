fcfd52a5bc209a5320ed844d57efcfdd4cfb96bd
kinga <kruppa.kinga@gmail.com>
2015-08-19 18:26:07 -0700

    pattern list

--- webui/app.js
+++ webui/app.js
@@ -17,7 +17,16 @@ $( document ).ready(function() {
                 alert("Error :(")
             }
         });
+
     });
+
+    getPatternList();
+
+    $(document).on("click", ".patternsBox input", function(){
+      setNewPattern($(this).parent().find('input[type="radio"]').val());
+    })
+
+
 
     function getLedStatus() {
         $.ajax({
@@ -31,14 +40,58 @@ $( document ).ready(function() {
             }
         });
     }
+
     function setButtons(status) {
-        status = status.toLowerCase()
+        status = status.toLowerCase();
         var activeElement = $("input[value='" + status + "']");
         activeElement.attr("checked", "checked");
         activeElement.parent().addClass('active');
     }
 
     function getPatternList(){
-      
+      $.ajax ({
+        url: serverUrl + "/settings",
+        dataType: 'json',
+        success: function(result) {
+          $.each(result['all_presets'], function(i, obj) {
+              var radioBtnData = "<p><label><input type=\"radio\" id=\" " + i + "\" name=\"patternGroup\" value=\"" + obj + "\" /> " + obj + "</label></p>";
+              $("#patternListBox").append(radioBtnData);
+              });
+          setActualPattern();
+       }
+     });
     }
+
+    function setNewPattern(newPattern){
+      $.ajax({
+          method: 'POST',
+          url:serverUrl + "/settings",
+          data: {"current_preset": newPattern},
+          dataType: 'json',
+          error: function(e) {
+              alert("Error :(")
+          }
+      });
+    }
+
+    function setActualPattern() {
+      $.ajax({
+          url:serverUrl + "/settings",
+          dataType: 'json',
+          success:function(result) {
+            var pattern = result['current_preset'];
+            var activeElement = $("input[value='" + pattern + "']");
+            activeElement.prop( "checked", true );
+          },
+          error: function(e) {
+              alert("Error :(")
+          }
+      });
+    }
+
+    $('.nav a').on('click', function(){
+    $(".btn-navbar").click(); //bootstrap 2.x
+    $(".navbar-toggle").click() //bootstrap 3.x by Richard
+    });
+
 });
